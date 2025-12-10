bb96bce9f1546214af1430deb9fbd0940ffd8ca0
Moetia Safitri Agustina <moetiasafitri@student.ub.ac.id>
2025-12-11 04:43:58 +0700

    install AOS

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,19 @@
+// ------------------------------------------
+// AXIOS
+// ------------------------------------------
 import axios from 'axios';
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// ------------------------------------------
+// BOOSTRAP + POPPER
+// ------------------------------------------
+
+// Popper wajib untuk Tooltip, Dropdown, Popover di Bootstrap
+import * as Popper from '@popperjs/core';
+window.Popper = Popper;
+
+// Import Bootstrap JS
+import 'bootstrap';
